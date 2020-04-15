50673fa5ef0f5d7d3cbebfb84317c5ee4b7f15c4
WesleyDev01 <josewesley12040@gmail.com>
2020-04-15 00:02:01 -0300

    Videos 01 e 02 (15/04)

---
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
@@ -0,0 +1,15 @@
+package com.eventoapp.eventoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.eventoapp.controllers"})
+public class EventoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoappApplication.class, args);
+	}
+
+}
